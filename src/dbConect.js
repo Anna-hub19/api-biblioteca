749de6d870c2705a98aa144eee4b0a09833b413e
anna-hub19 <annabastosbeatriz@gmail.com>
2024-01-04 12:58:15 -0300

    aula 2 adicionando erros

--- src/dbConect.js
+++ src/dbConect.js
@@ -1,10 +1,7 @@
 import mongoose from "mongoose";
 
-async function conectaNaDatabase() {
-    mongoose.connect(process.env.STRING_CONEXAO_DB);
-      
-    return mongoose.connection;
-}
-  
-export default conectaNaDatabase;
+mongoose.connect(process.env.STRING_CONEXAO_DB);
 
+let db = mongoose.connection;
+
+export default db;
